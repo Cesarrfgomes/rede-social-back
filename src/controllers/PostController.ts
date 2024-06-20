05945c1f0492d0e31c1011469c45758b0b119b86
unknown <filipegomes012642@gmail.com>
2024-06-20 10:28:44 -0300

    enpoint para cadastrar postagem criado

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,2 +1,30 @@
 import {Request, Response} from 'express'
 import Post from "../models/Posts"
+import User from '../models/Users'
+import mongoose from 'mongoose'
+
+export class PostController{
+    async create(req:Request, res:Response){
+        const {user_id, description, images} = req.body
+
+        try {
+            const user = await User.findById(user_id)
+
+            if(!user){
+                return res.status(404).json({message: "Usuário não encontrado"})
+            }
+
+            const newPost = await Post.create({
+                user_id: user._id,
+                description,
+                images,
+                likes: 0,
+                comments: []
+            })
+
+            return res.status(201).json(newPost)
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor!"})
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import { UserController } from './controllers/UserController'
+import { PostController } from './controllers/PostController'
 
 const routes = Router()
 
@@ -8,5 +9,7 @@ routes.get('/user', new UserController().get)
 routes.get('/user/:id', new UserController().show)
 routes.put('/user/:id', new UserController().update)
 routes.patch('/user/:id/inactive', new UserController().inactive)
+
+routes.post('/post', new PostController().create)
 
 export default routes
