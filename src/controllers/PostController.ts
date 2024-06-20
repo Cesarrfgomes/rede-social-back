54e9e521d3f923c820f42560cb78823e830ecb0b
unknown <filipegomes012642@gmail.com>
2024-06-20 10:56:57 -0300

    endpoint para comentar postagem criado

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -112,4 +112,30 @@ export class PostController{
             return res.status(500).json({message: "Erro interno do servidor!"})
         }
     }
+
+    async commentPost(req: Request, res: Response){
+        const {id} = req.params
+        const {description,} = req.body
+        
+        try {
+            const post = await Post.findById(id)
+
+            if(!post){
+                return res.status(404).json({message: "Postagem não encontrada!"})
+            }
+            
+            await Post.updateOne({_id: id}, {
+                $push: {
+                    comments: {
+                        _id: new mongoose.Types.ObjectId(),
+                        description
+                    }
+                }
+            })
+
+            return res.status(204).json()
+        } catch (error) {
+            return res.status(500).json({message: "Erro interno do servidor!"})
+        }
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ routes.get('/post/:id', new PostController().show)
 routes.patch('/post/:id', new PostController().update)
 routes.delete('/post/:id', new PostController().delete)
 routes.patch('/post/:id/like', new PostController().like)
+routes.patch('/post/:id/comment', new PostController().commentPost)
 
 
 export default routes
