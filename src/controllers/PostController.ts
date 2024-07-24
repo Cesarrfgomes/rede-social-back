6b6051ae30c5d0ace71681d1f6a9e64e80c2f362
César Filipe <filipegomes012642@gmail.com>
2024-07-24 13:30:26 -0300

    .

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -30,7 +30,19 @@ export class PostController{
 
     async get(req:Request, res: Response){
         try {
-            const posts = await Post.find()
+            const posts = await Post.aggregate([
+                {
+                    $lookup: {
+                                from: 'users',
+                                localField: 'user_id',
+                                foreignField: '_id',
+                                as: 'user'
+                            },
+                    $addFields:{
+                                user: {$first: '$user'}
+                            }
+                }
+            ])
 
             return res.status(200).json(posts)
         } catch (error) {
